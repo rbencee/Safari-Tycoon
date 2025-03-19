31b0f74966efa43b6a1a4a73427338eb54542ac6
Bence <x7bdkq@inf.elte.hu>
2025-03-19 19:07:03 +0100

    Update2

--- lwjgl3/src/main/java/io/github/safari/lwjgl3/maingame/GameView.java
+++ lwjgl3/src/main/java/io/github/safari/lwjgl3/maingame/GameView.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
