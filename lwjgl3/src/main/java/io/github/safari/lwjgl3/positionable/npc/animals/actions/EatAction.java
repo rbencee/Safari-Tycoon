4bfb4de3a8dfc382c3094f445d5969c357518c4e
Karina <nlubn3@inf.elte.hu>
2025-04-23 22:30:26 +0200

    állatok alszanak evés után

--- lwjgl3/src/main/java/io/github/safari/lwjgl3/positionable/npc/animals/actions/EatAction.java
+++ lwjgl3/src/main/java/io/github/safari/lwjgl3/positionable/npc/animals/actions/EatAction.java
@@ -2,7 +2,6 @@ package io.github.safari.lwjgl3.positionable.npc.animals.actions;
 
 import com.badlogic.gdx.scenes.scene2d.Action;
 import io.github.safari.lwjgl3.positionable.npc.animals.AnimalImpl;
-import io.github.safari.lwjgl3.positionable.npc.animals.Herd;
 
 public class EatAction extends Action implements CloneableAction {
 
